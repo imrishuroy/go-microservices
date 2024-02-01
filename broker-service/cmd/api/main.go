b592b649486f6e12ee344e0e54216915378e92c6
imrishuroy <rishurkumar.prince@gmail.com>
2024-02-01 23:16:59 +0530

    project setup, FE & Broker service

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+	log.Printf("Starting broker service on port %s\n", webPort)
+
+	// define http server
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	// start http server
+	if err := server.ListenAndServe(); err != nil {
+		log.Panic(err)
+	}
+}
---
+++ broker-service/go.mod
@@ -0,0 +1,9 @@
+module boker
+
+go 1.21.6
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.0.11
+	github.com/go-chi/cors v1.2.1
+)
